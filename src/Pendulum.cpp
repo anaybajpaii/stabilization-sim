7fda4ba643cbaec430f24a435ba18c020dff22dd
Anay Bajpai <anay.bajpai25@gmail.com>
2026-09-14 15:34:26 +0530

    Add Pendulum class with uncontrolled gravity physics

---
+++ src/Pendulum.cpp
@@ -0,0 +1,25 @@
+#include "Pendulum.h"
+#include <cmath>
+
+Pendulum::Pendulum(double initialAngle, double initialAngularVelocity)
+{
+    angle = initialAngle;
+    angularVelocity = initialAngularVelocity;
+}
+
+void Pendulum::update(double dt)
+{
+    double angularAcceleration = (GRAVITY / LENGTH) * sin(angle);
+    angularVelocity = angularVelocity+(angularAcceleration*dt);
+    angle = angle + angularVelocity * dt;
+}
+    
+double Pendulum::getAngle() const
+{
+    return angle;
+}
+
+double Pendulum::getAngularVelocity() const
+{
+    return angularVelocity;
+}
---
+++ src/Pendulum.h
@@ -0,0 +1,19 @@
+#ifndef PENDULUM_H
+#define PENDULUM_H
+
+class Pendulum 
+{
+public:
+    Pendulum(double initialAngle, double initialAngularVelocity);
+    void update(double dt);
+    double getAngle() const;
+    double getAngularVelocity() const;
+
+private:
+    double angle;
+    double angularVelocity;
+    static constexpr double GRAVITY = 9.81;
+    static constexpr double LENGTH = 1.0;
+};
+
+#endif
